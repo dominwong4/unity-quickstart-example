2068a8415dc02439a23f73c8cf284db0470a508a
dominic wong <dom@valoot.com>
2017-12-11 21:30:38 +0800

    change to installation

--- Assets/Back4app/Back4appConnectionSample.cs
+++ Assets/Back4app/Back4appConnectionSample.cs
@@ -7,10 +7,15 @@ public class Back4appConnectionSample : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		ParseObject gameScore = new ParseObject ("GameSocre");
-		gameScore["score"] = 9999;
-		gameScore["playerName"] = "Unity3d Latest Parse Version";
-		Task saveTask = gameScore.SaveAsync();
+		// Uncomment below if you want to test the database
+
+//		ParseObject gameScore = new ParseObject ("GameSocre");
+//		gameScore["score"] = 9999;
+//		gameScore["playerName"] = "Unity3d Latest Parse Version";
+//		Task saveTask = gameScore.SaveAsync();
+
+		// Saving the Parse Installation
+		ParseInstallation.CurrentInstallation.SaveAsync ();
 	}
 
 	// Update is called once per frame
